04b337c003193d6e7bcc7ec7c639819fa393ed3e
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-06-07 14:23:11 -0700

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { Template } = require("webpack");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        bundle: path.resolve(__dirname, "src/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack App",
+            filename: "index.html",
+            template: "src/template.html"
+        })
+    ]
+}
