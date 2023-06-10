7463347c1c2caaff61a1fdbc2d84cbb3f7bc1871
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-06-09 21:08:44 -0700

    Created test case for isSunk function

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -2,6 +2,19 @@ import { ship } from "../ship"
 
 describe("Ship factory", () => {
     test("should hit ship", () => {
+        ship.hit()
         expect(ship.hit()).toBe(2)
+    })
+    test("length of ship", () => {
+        expect(ship.getLength(3)).toBe(3)
+    })
+    test("ship has not been hit", () => {
+        expect(ship.isSunk()).toBe(false)
+    })
+    test("ship has been sunk", () => {
+        ship.hit()
+        ship.hit()
+        ship.hit()
+        expect(ship.isSunk()).toBe(true)
     })
 })
