51ac1c4f6b1a343c558bc970c7f374b10bb69c96
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-06-15 20:27:06 -0700

    Fixed typo in test

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -2,12 +2,12 @@ import ship from "../ship";
 import gameboard from "../gameboard";
 
 describe("Gameboard factory", () => {
-    test("Does gameboard has 100 cells", () => {
+    test("Does gameboard have 100 cells", () => {
         expect(gameboard.cellCount).toBe(100)
     })
-    // test("Can place ship at specific coordinates", () => {
-    //     expect(gameboard.placeShip([0, 0], ship)).toEqual(true)
-    // })
+    test("Can place ship at specific coordinates", () => {
+        expect(gameboard.placeShip([0, 0], ship)).toEqual(true)
+    })
     // test("Determine if attack hits ship", () => {
     //     expect(gameboard.receiveAttack([1, 0], ship.hit())).toEqual(true)
     // })
