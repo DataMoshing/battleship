eaae3cfe2955f3784125ef877a41bf67dbcb6521
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-06-22 00:31:05 -0700

    Created logic to place ships

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,28 +1,57 @@
-// import { createShip } from "./ship"
+import testShip from "./ship"
 
 const Gameboard = () => {
     const rows = 10
-    const colums = 10
-
+    const columns = 10
     const board = []
-    // Need to reduce array amount to a single value
-    const cellCount = board.reduce((row, columns) => row + columns.length, 0)
+
+    console.log(board)
 
     const getBoard = () => [...board]
 
     for (let i = 0; i < rows; i += 1) {
         board[i] = []
-        for (let j = 0; j < colums; j += 1) {
+        for (let j = 0; j < columns; j += 1) {
             board[i][j] = null
         }
     }
 
-    const placeShip = (x, y) => {
-    }
+    // Need to reduce array amount to a single value
+    const cellCount = getBoard().reduce((row, col) => row + col.length, 0)
 
+    const placeShip = (x, y, ship, direction) => {
+        const shipsLength = ship.getLength();
+        if (direction === "horizontal") {
+            if (y + shipsLength > columns) {
+                console.log("Cannot place ship horizontally. Out of bounds.");
+                return false;
+            }
+            // Places ship horizontally
+            for (let i = 0; i < shipsLength; i += 1) {
+                board[x][y + i] = ship.name;
+            }
+
+        } else if (direction === "vertical") {
+            if (x + shipsLength > rows) {
+                console.log("Cannot place ship vertically. Out of bounds.");
+                return false;
+            }
+            // Places ship vertically
+            for (let i = 0; i < shipsLength; i += 1) {
+                board[x + i][y] = ship.name;
+            }
+        } else {
+            console.log("Invalid direction. Please specify 'horizontal' or 'vertical'.");
+            return false
+        }
+        return true
+    }
     return { placeShip, getBoard, cellCount }
 }
-
 const gameboard = Gameboard()
 
+gameboard.placeShip(2, 4, testShip, "horizontal")
+
+
 export default gameboard
+
