f134c1a961d18b73282bc2d2c35d9190106e2be3
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-06-24 17:35:27 -0700

    Created test case for receiving attacks

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,17 +1,14 @@
 import gameboard from "../gameboard"
 import testShip from "../ship"
 
-
-
 describe("Gameboard factory", () => {
     test("Does gameboard have 100 cells", () => {
         expect(gameboard.getBoard().reduce((acc, column) => acc + column.length, 0)).toBe(100)
     })
     test("Can place ships at specific coordinates", () => {
-        gameboard.placeShip(2, 4, testShip, "horizontal")
-        expect(gameboard.placeShip(2, 4, testShip, "horizontal")).toEqual(true)
+        expect(gameboard.placeShipsVertical(2, 4, testShip, "horizontal")).toEqual(true)
     })
-    // test("Determine if attack hits ship", () => {
-    //     expect(gameboard.receiveAttack([1, 0], ship.hit())).toEqual(true)
-    // })
+    test("Determine if attack hits ship", () => {
+        expect(gameboard.receiveAttack(2, 4, testShip)).toEqual(true)
+    })
 })
