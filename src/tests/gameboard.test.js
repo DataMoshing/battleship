9667bee61d3a75f554f289f10a688037b33492b9
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-06-27 17:28:37 -0700

    Created test case for overlapping ships

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,14 +1,18 @@
 import gameboard from "../gameboard"
-import testShip from "../ship"
+import { testShip, testShip2 } from "../ship"
 
 describe("Gameboard factory", () => {
     test("Does gameboard have 100 cells", () => {
         expect(gameboard.getBoard().reduce((acc, column) => acc + column.length, 0)).toBe(100)
     })
     test("Can place ships at specific coordinates", () => {
-        expect(gameboard.placeShipsVertical(2, 4, testShip, "horizontal")).toEqual(true)
+        expect(gameboard.placeShipsVertical(2, 4, testShip, "vertical")).toEqual(true)
     })
     test("Determine if attack hits ship", () => {
         expect(gameboard.receiveAttack(2, 4, testShip)).toEqual(true)
+    })
+    test("Ships cannot overlap", () => {
+        expect(gameboard.placeShipsVertical(2, 4, testShip, "vertical")).toEqual(true)
+        expect(gameboard.placeShipsHorizontal(2, 4, testShip2, "horizontal")).toEqual(false)
     })
 })
