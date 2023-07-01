9bfeff8b59ea3e2b84ccfa0081aa574d493b7afd
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-07-01 16:45:53 -0700

    Removed dependency of tests

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,20 +1,22 @@
 import gameboard from "../gameboard"
-import { testShip, testShip2 } from "../ship"
+import Ship from "../ship"
 
 describe("Gameboard factory", () => {
     test("Does gameboard have 100 cells", () => {
         expect(gameboard.getBoard().reduce((acc, column) => acc + column.length, 0)).toBe(100)
     })
-    test("Can place ships at specific coordinates", () => {
-        expect(gameboard.placeShipsVertical(1, 4, testShip, "vertical")).toEqual(true)
-    })
     test("Ships cannot overlap", () => {
+        const testShip = Ship(3, "Boat")
+        const testShip2 = Ship(4, "Boat")
+        expect(gameboard.placeShipsVertical(1, 4, testShip, "vertical")).toEqual(true)
         expect(gameboard.placeShipsHorizontal(1, 4, testShip2, "horizontal")).toEqual(false)
     })
     test("Ship cannot be placed outside of board", () => {
+        const testShip = Ship(3, "Boat")
         expect(gameboard.placeShipsHorizontal(2, 10, testShip, "horizontal")).toEqual(false)
     })
     test("Determine if attack hits ship", () => {
+        const testShip = Ship(3, "Boat")
         expect(gameboard.receiveAttack(2, 4, testShip)).toEqual(true)
     })
     test("Check if all ships have been sunk", () => {
