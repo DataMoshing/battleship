b0cbba2c0f30cae6a180827517dda10b62b744b9
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-07-02 20:51:33 -0700

    Created test case for not allowing attacks in same spot

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,25 +1,38 @@
-import gameboard from "../gameboard"
+import Gameboard from "../gameboard"
 import Ship from "../ship"
 
 describe("Gameboard factory", () => {
     test("Does gameboard have 100 cells", () => {
+        const gameboard = Gameboard()
         expect(gameboard.getBoard().reduce((acc, column) => acc + column.length, 0)).toBe(100)
     })
     test("Ships cannot overlap", () => {
         const testShip = Ship(3, "Boat")
         const testShip2 = Ship(4, "Boat")
-        expect(gameboard.placeShipsVertical(1, 4, testShip, "vertical")).toEqual(true)
-        expect(gameboard.placeShipsHorizontal(1, 4, testShip2, "horizontal")).toEqual(false)
+        const gameboard = Gameboard()
+        expect(gameboard.placeVertical(1, 4, testShip, "vertical")).toEqual(true)
+        expect(gameboard.placeHorizontal(1, 4, testShip2, "horizontal")).toEqual(false)
     })
     test("Ship cannot be placed outside of board", () => {
         const testShip = Ship(3, "Boat")
-        expect(gameboard.placeShipsHorizontal(2, 10, testShip, "horizontal")).toEqual(false)
+        const gameboard = Gameboard()
+        expect(gameboard.placeHorizontal(2, 10, testShip, "horizontal")).toEqual(false)
     })
     test("Determine if attack hits ship", () => {
         const testShip = Ship(3, "Boat")
+        const gameboard = Gameboard()
+        gameboard.placeHorizontal(2, 4, testShip, "horizontal")
         expect(gameboard.receiveAttack(2, 4, testShip)).toEqual(true)
     })
     test("Check if all ships have been sunk", () => {
+        const gameboard = Gameboard()
         expect(gameboard.allSunk()).toEqual(true)
+    })
+    test("Cannot allow to hit in same spot", () => {
+        const testShip = Ship(3, "Boat")
+        const gameboard = Gameboard()
+        gameboard.placeHorizontal(2, 4, testShip, "horizontal")
+        expect(gameboard.receiveAttack(2, 4, testShip)).toEqual(true)
+        expect(gameboard.receiveAttack(2, 4, testShip)).toEqual(false)
     })
 })
