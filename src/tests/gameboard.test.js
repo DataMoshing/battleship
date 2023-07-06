d1670015dd623eea5a62cd14512cbeeafe5e34ab
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-07-06 00:42:24 -0700

    Refactored test cases

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,5 +1,5 @@
 import Gameboard from "../gameboard"
-import Ship from "../ship"
+import createShip from "../ship"
 
 describe("Gameboard factory", () => {
     test("Does gameboard have 100 cells", () => {
@@ -7,19 +7,19 @@ describe("Gameboard factory", () => {
         expect(gameboard.getBoard().reduce((acc, column) => acc + column.length, 0)).toBe(100)
     })
     test("Ships cannot overlap", () => {
-        const testShip = Ship(3, "Boat")
-        const testShip2 = Ship(4, "Boat")
+        const testShip = createShip(3, "Boat")
+        const testShip2 = createShip(4, "Boat")
         const gameboard = Gameboard()
         expect(gameboard.placeVertical(1, 4, testShip, "vertical")).toEqual(true)
         expect(gameboard.placeHorizontal(1, 4, testShip2, "horizontal")).toEqual(false)
     })
     test("Ship cannot be placed outside of board", () => {
-        const testShip = Ship(3, "Boat")
+        const testShip = createShip(3, "Boat")
         const gameboard = Gameboard()
         expect(gameboard.placeHorizontal(2, 10, testShip, "horizontal")).toEqual(false)
     })
     test("Determine if attack hits ship", () => {
-        const testShip = Ship(3, "Boat")
+        const testShip = createShip(3, "Boat")
         const gameboard = Gameboard()
         gameboard.placeHorizontal(2, 4, testShip, "horizontal")
         expect(gameboard.receiveAttack(2, 4, testShip)).toEqual(true)
@@ -29,7 +29,7 @@ describe("Gameboard factory", () => {
         expect(gameboard.allSunk()).toEqual(true)
     })
     test("Cannot allow to hit in same spot", () => {
-        const testShip = Ship(3, "Boat")
+        const testShip = createShip(3, "Boat")
         const gameboard = Gameboard()
         gameboard.placeHorizontal(2, 4, testShip, "horizontal")
         expect(gameboard.receiveAttack(2, 4, testShip)).toEqual(true)
