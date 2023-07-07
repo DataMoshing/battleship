d38f5b992f14b53432d6b5a2c0ede2d59d892c48
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-07-07 13:11:41 -0700

    Renamed factory function for brevity

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,23 +1,23 @@
-import Ship from "../ship"
+import createShip from "../ship"
 
 describe("Ship factory", () => {
     test("should hit ship", () => {
-        const testShip = Ship(3, "Boat")
+        const testShip = createShip(3, "Boat")
         expect(testShip.hit()).toBe(1)
     })
     test("length of ship", () => {
-        const testShip = Ship(3, "Boat")
+        const testShip = createShip(3, "Boat")
         expect(testShip.getLength(3)).toBe(3)
     })
     test("Ship has been sunk", () => {
-        const testShip = Ship(3, "Boat")
+        const testShip = createShip(3, "Boat")
         testShip.hit()
         testShip.hit()
         testShip.hit()
         expect(testShip.isSunk()).toBe(true)
     })
     test("Ship has not been sunk", () => {
-        const testShip = Ship(3, "Boat")
+        const testShip = createShip(3, "Boat")
         testShip.hit()
         expect(testShip.isSunk()).toBe(false)
     })
