fa74faf9f533127176a38f41fe3394530387f9bf
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-07-25 16:27:40 -0700

    Created test case for placing ships randomly

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,16 +1,15 @@
-import { createPlayer, createComputer } from "../player"
+import { createComputer } from "../player"
 import createShip from "../ship"
 
 describe("Player factory", () => {
-    test("Can send attacks to enemy board", () => {
-        const player = createPlayer()
+    test("Computer can randomly place ships horizontally", () => {
         const computer = createComputer()
         const testShip = createShip(3, "Boat")
-        computer.computerGameboard.placeHorizontal(1, 4, testShip, "horizontal")
-        player.setEnemyBoard(computer)
-        player.sendAttack(1, 4, computer)
-        player.sendAttack(1, 5, computer)
-        player.sendAttack(1, 6, computer)
-        expect(computer.computerGameboard.allSunk()).toEqual(true)
+        expect(computer.placeShipHorizontal(testShip)).toEqual(true)
+    })
+    test("Computer can randomly place ships verically", () => {
+        const computer = createComputer()
+        const testShip = createShip(2, "Boat")
+        expect(computer.placeShipVertical(testShip)).toEqual(true)
     })
 })
