c437afc6a37518ddfed643786d2bbbb243f3a37e
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-08-08 15:54:05 -0700

    Created test case to prevent out of bounds attacks

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -6,18 +6,22 @@ describe("Gameboard factory", () => {
         const gameboard = Gameboard()
         expect(gameboard.getBoard().reduce((acc, column) => acc + column.length, 0)).toBe(100)
     })
+    test("Check if coordinates are valid", () => {
+        const gameboard = Gameboard()
+        expect(gameboard.validCoords(8, 10)).toEqual(false)
+    })
+    test("Check if ship is inbounds of gameboard", () => {
+        const gameboard = Gameboard()
+        const testShip = createShip(3, "Boat")
+        expect(gameboard.shipIsInbounds(8, 10, testShip)).toEqual(false)
+    })
     test("Ships cannot overlap", () => {
         const testShip = createShip(3, "Boat")
         const testShip2 = createShip(4, "Boat")
         const gameboard = Gameboard()
-        expect(gameboard.placeVertical(1, 4, testShip)).toEqual(true)
-        expect(gameboard.placeHorizontal(1, 4, testShip2)).toEqual(false)
-    })
-    test("Ship cannot be placed outside of board", () => {
-        const testShip = createShip(3, "Boat")
-        const gameboard = Gameboard()
-        gameboard.placeHorizontal(8, 10, testShip)
-        expect(gameboard.validCoords(8, 10)).toEqual(false)
+        gameboard.placeVertical(1, 4, testShip)
+        gameboard.placeHorizontal(1, 4, testShip2)
+        expect(gameboard.doShipsCollide(1, 4, testShip2)).toEqual(false)
     })
     test("Determine if attack hits ship", () => {
         const testShip = createShip(3, "Boat")
@@ -35,5 +39,9 @@ describe("Gameboard factory", () => {
         gameboard.placeVertical(2, 2, testShip)
         expect(gameboard.receiveAttack(2, 2)).toEqual(true)
         expect(gameboard.canShipBeHitAgain(2, 2)).toEqual(false)
+    })
+    test("Cannot attack outside of gameboard", () => {
+        const gameboard = Gameboard()
+        expect(gameboard.receiveAttack(10, 11)).toEqual(false)
     })
 })
