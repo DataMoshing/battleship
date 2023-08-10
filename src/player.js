ef750996a7e66a9cde0c55e5a00d6b9f24698d8a
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-08-10 00:56:11 -0700

    Created logic on computer to attack player

--- src/player.js
+++ src/player.js
@@ -1,10 +1,10 @@
-import Gameboard from "./gameboard"
 import createShip from "./ship"
-// import createShip from "./ship"
+import Gameboard from "./gameboard"
 
 const createComputer = () => {
     const computerGameboard = Gameboard()
-    // const getCompBoard = computerGameboard.getBoard()
+    const getCompBoard = computerGameboard.getBoard()
+    console.log(getCompBoard)
 
     const placeShipHorizontal = (ship) => {
         let x;
@@ -14,46 +14,57 @@ const createComputer = () => {
             y = Math.floor((Math.random() * 9));
         }
         while
-            (computerGameboard.placeHorizontal(x, y, ship, "horizontal") === false);
-        computerGameboard.placeHorizontal(x, y, ship, "horizontal")
+            (computerGameboard.placeHorizontal(x, y, ship) === false);
+        computerGameboard.placeHorizontal(x, y, ship)
         return true
     }
 
     const placeShipVertical = (ship) => {
-        let x;
-        let y;
+        let x
+        let y
         do {
-            x = Math.floor((Math.random() * 9));
-            y = Math.floor((Math.random() * 9));
+            x = Math.floor((Math.random() * 9))
+            y = Math.floor((Math.random() * 9))
         }
         while
-            (computerGameboard.placeVertical(x, y, ship) === false);
+            (computerGameboard.placeVertical(x, y, ship) === false)
         computerGameboard.placeVertical(x, y, ship)
         return true
     }
 
     const setEnemyBoard = (player) => player.getPlayerBoard
 
-    return { computerGameboard, setEnemyBoard, placeShipHorizontal, placeShipVertical }
+    const sendAttack = (player) => {
+        let x
+        let y
+        do {
+            x = Math.floor((Math.random() * 9))
+            y = Math.floor((Math.random() * 9))
+        }
+        while (player.playerGameboard.receiveAttack(x, y))
+        return true
+    }
+
+    return { computerGameboard, setEnemyBoard, placeShipHorizontal, placeShipVertical, sendAttack }
 }
 
 const createPlayer = (name) => {
     const getName = () => name
-    const gameboard = Gameboard()
+    const playerGameboard = Gameboard()
     // const getPlayerBoard = gameboard.getBoard()
     // console.log(getPlayerBoard)
 
     const placeShipHorizontal = (x, y, ship) => {
-        gameboard.placeHorizontal(x, y, ship)
+        playerGameboard.placeHorizontal(x, y, ship)
     }
 
     const placeShipVertical = (x, y, ship) => {
-        gameboard.placeVertical(x, y, ship)
+        playerGameboard.placeVertical(x, y, ship)
     }
 
-    const makeAttack = (x, y) => {
-        gameboard.receiveAttack(x, y)
-    }
+    // const makeAttack = (x, y) => {
+    //     playerGameboard.receiveAttack(x, y)
+    // }
 
     const setEnemyBoard = (comp) => comp.getCompBoard
 
@@ -61,7 +72,7 @@ const createPlayer = (name) => {
         comp.computerGameboard.receiveAttack(x, y)
     }
 
-    return { getName, placeShipHorizontal, placeShipVertical, makeAttack, /* getPlayerBoard, */ setEnemyBoard, sendAttack }
+    return { getName, placeShipHorizontal, placeShipVertical, setEnemyBoard, sendAttack, playerGameboard }
 }
 
 const computer = createComputer()
@@ -70,8 +81,10 @@ player.setEnemyBoard(computer)
 computer.setEnemyBoard(player)
 const testShip = createShip(3, "1")
 const testShip2 = createShip(3, "2")
+player.sendAttack(1, 2, computer)
 
 computer.placeShipVertical(testShip)
 computer.placeShipHorizontal(testShip2)
+computer.sendAttack(player)
 
 export { createComputer, createPlayer }
