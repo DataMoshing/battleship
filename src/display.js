6b0ce9bf290f3c1855369e569cb9cf62297fbe3c
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-08-14 17:25:47 -0700

    Pushing current code to repo

--- src/display.js
+++ src/display.js
@@ -1,38 +1,41 @@
-// import createGame from "./game"
+import createShip from "./ship";
 
 const playerContainer = document.querySelector(".player-container")
-// const computerContainer = document.querySelector(".computer-container")
-// const table = document.querySelector(".table")
-const cells = document.getElementsByClassName("cells")
+const shipContainer = document.querySelector(".ship-container")
+const cells = document.getElementsByClassName("board-cell")
 
 const columns = 10;
 const rows = 10
-const displayPlayerGrid = (x, y) => {
+const displayPlayerGrid = () => {
     for (let i = 0; i < rows; i += 1) {
         const row = document.createElement("tr")
+        row.classList.add("board-row")
         playerContainer.append(row)
         for (let j = 0; j < columns; j += 1) {
             const cell = document.createElement("td")
+            cell.classList.add("board-cell")
             cell.setAttribute("x", i)
             cell.setAttribute("y", j)
             row.append(cell)
         }
     }
 }
-// for (let i = 0; i < 100; i += 1) {
-//     const cell = document.createElement("div")
-//     cell.classList.add("cells")
-//     playerContainer.append(cell)
+
+displayPlayerGrid()
+
+// const carrierDisplay = () => {
+//     const carrier = createShip(5, "Carrier")
+//     const carrierDiv = document.createElement("div")
+//     carrierDiv.classList.add("carrier")
+//     carrierDiv.setAttribute("draggable", true)
+
+//     for (let i = 0; i < carrier.getLength(); i += 1) {
+//         const carrierCell = document.createElement("div")
+//         carrierCell.classList.add("carrier-cell")
+//         carrierCell.setAttribute("data-index", i)
+//         carrierDiv.append(carrierCell)
+//         shipContainer.append(carrierDiv)
+//     }
 // }
 
-displayPlayerGrid(10, 10)
-
-
-for (let i = 0; i < cells.length; i += 1) {
-    cells[i].setAttribute("y", 0)
-    cells[i].setAttribute("x", i)
-}
-
-
-
-
+// carrierDisplay()
