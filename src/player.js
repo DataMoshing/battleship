d39962958365f932df9d99fb6ef0885cb28416da
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-08-15 19:33:42 -0700

    Fixed player functions not returning values

--- src/player.js
+++ src/player.js
@@ -1,10 +1,8 @@
-import createShip from "./ship"
 import Gameboard from "./gameboard"
 
 const createComputer = () => {
     const computerGameboard = Gameboard()
-    const getCompBoard = computerGameboard.getBoard()
-    console.log(getCompBoard)
+    // const getCompBoard = computerGameboard.getBoard()
 
     const placeShipHorizontal = (ship) => {
         let x;
@@ -51,40 +49,16 @@ const createComputer = () => {
 const createPlayer = (name) => {
     const getName = () => name
     const playerGameboard = Gameboard()
-    // const getPlayerBoard = gameboard.getBoard()
-    // console.log(getPlayerBoard)
 
-    const placeShipHorizontal = (x, y, ship) => {
-        playerGameboard.placeHorizontal(x, y, ship)
-    }
+    const placeShipHorizontal = (x, y, ship) => playerGameboard.placeHorizontal(x, y, ship)
 
-    const placeShipVertical = (x, y, ship) => {
-        playerGameboard.placeVertical(x, y, ship)
-    }
-
-    // const makeAttack = (x, y) => {
-    //     playerGameboard.receiveAttack(x, y)
-    // }
+    const placeShipVertical = (x, y, ship) => playerGameboard.placeVertical(x, y, ship)
 
     const setEnemyBoard = (comp) => comp.getCompBoard
 
-    const sendAttack = (x, y, comp) => {
-        comp.computerGameboard.receiveAttack(x, y)
-    }
+    const sendAttack = (x, y, comp) => comp.computerGameboard.receiveAttack(x, y)
 
     return { getName, placeShipHorizontal, placeShipVertical, setEnemyBoard, sendAttack, playerGameboard }
 }
-
-const computer = createComputer()
-const player = createPlayer("Player1")
-player.setEnemyBoard(computer)
-computer.setEnemyBoard(player)
-const testShip = createShip(3, "1")
-const testShip2 = createShip(3, "2")
-player.sendAttack(1, 2, computer)
-
-computer.placeShipVertical(testShip)
-computer.placeShipHorizontal(testShip2)
-computer.sendAttack(player)
 
 export { createComputer, createPlayer }
